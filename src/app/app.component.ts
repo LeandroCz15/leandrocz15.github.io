6b1fef4a450afe5e922b576bd3f6aec46e852755
Leandro <leandrocazorla15@gmail.com>
2023-02-05 15:03:01 -0300

    Arreglo a la navbar.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { event } from 'jquery';
 import { AppCredentials } from './classes/app-credentials/app-credentials';
 import { Issue } from './classes/issue/issue';
 import { JiraLoginCredentials } from './classes/jira-login-credentials/jira-login-credentials';
@@ -8,7 +9,7 @@ import { JiraLoginCredentials } from './classes/jira-login-credentials/jira-logi
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent implements AfterViewInit, OnInit {
   title = 'Leandro\'s-proyect';
   selectedPageIndex : string = "0";
   currentIssue : Issue = new Issue("", "", "", 0, 0, "", -1);
@@ -62,24 +63,54 @@ export class AppComponent implements AfterViewInit {
     document.getElementById(this.selectedPageIndex)!.style.backgroundColor = "#0d6efd";
   }
 
+  ngOnInit(){
+    this.resizeTopNavbarOnWindowChange()
+    this.resizeTopNavbarOnInit();
+  }
+
   toggleSidebar(){
+    //toggler function of left sidebar
+    let newWidth: number;
     let sideBar: HTMLElement | null = document.getElementById("side-bar");
     let sideBarButtons: HTMLCollectionOf<HTMLElement> = document.getElementsByClassName("btn-text") as HTMLCollectionOf<HTMLElement>;
     let sideBarToggler: HTMLElement | null = document.getElementById("side-bar-toggler");
     if(!this.sideBarToggled) {
       sideBarToggler!.setAttribute("src", "assets/right-arrow.svg");
-      sideBar!.style.width = "55px";
+      newWidth = 55;
+      sideBar!.style.width = newWidth + "px";
       for(let i = 0 ; i < sideBarButtons.length ; i++){
         sideBarButtons.item(i)!.style.display = "none"; 
       }
     } else {
       sideBarToggler!.setAttribute("src", "assets/left-arrow.svg");
-      sideBar!.style.width = "155px";
+      newWidth = 155;
+      sideBar!.style.width = newWidth + "px";
       for(let i = 0 ; i < sideBarButtons.length ; i++){
         sideBarButtons.item(i)!.style.display = "inline"; 
       }
     }
     this.sideBarToggled = !this.sideBarToggled;
+    this.resizeTopNavbarOnSideBarChange(newWidth);
+  }
+
+  resizeTopNavbarOnSideBarChange(newSize: number){
+    let navBar: HTMLElement | null = document.getElementById("navbar");
+    navBar!.style.width = window.innerWidth - newSize + "px";
+  }
+
+  resizeTopNavbarOnInit(){
+    //function to handle the size of the top navbar, since i didnt find a way to make it look good when adding property fixed to the navbar
+    let navBar: HTMLElement | null = document.getElementById("navbar");
+    let sideBar: HTMLElement | null = document.getElementById("side-bar");
+    navBar!.style.width = window.innerWidth - sideBar!.clientWidth + "px";
+  }
+
+  resizeTopNavbarOnWindowChange(){
+    window.onresize = function(){
+      let navBar: HTMLElement | null = document.getElementById("navbar");
+      let sideBar: HTMLElement | null = document.getElementById("side-bar");
+      navBar!.style.width = window.innerWidth - sideBar!.clientWidth + "px";
+    }
   }
 
 }
